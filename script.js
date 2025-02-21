4a5884892f4c208f4f66d5e7348569c2eca1620c
parag lakum <paragl.albiorix@gmail.com>
2025-02-21 16:46:21 +0530

    Task1.2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Declare variables of different data types
-let String = "Hello, World!";  
-let Number = 42;               
-let Boolean = true;            
-let Object = { name: "Alice", age: 25 };  
-let Array = [1, 2, 3, 4, 5];   
+// let String = "Hello, World!";  
+// let Number = 42;               
+// let Boolean = true;            
+// let Object = { name: "Alice", age: 25 };  
+// let Array = [1, 2, 3, 4, 5];   
+
+//swap two Variables
+
+let a = 10;
+let b = 20;
+console.log("before");
+console.log("a =",a,"b =",b);
+
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("after");
+console.log("a =",a,"b =",b);
 
